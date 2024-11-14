3571ff60b20de20f1d26176648a581d66bff96de
Jay <74466030+JayHawkinsMTU@users.noreply.github.com>
2024-11-13 21:51:49 -0500

    Added background to houghton level
    I hate using godot's parallax background, it's finnicky as hell. Good luck when adding it to other levels.

--- Demo/scripts/parallax_scroll.gd
+++ Demo/scripts/parallax_scroll.gd
@@ -1,5 +1,7 @@
 extends ParallaxBackground
 
+@export var speed: float = 1
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	scroll_base_offset -= Vector2(100, 0) * delta
+	scroll_base_offset -= Vector2(100, 0) * delta * speed
