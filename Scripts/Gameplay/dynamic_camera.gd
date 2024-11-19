0a3f721c087bb8b252dc4001b4d08a214465968d
jrhawkinMTU <hawkinsjr27@gmail.com>
2024-11-19 18:07:18 -0500

    Added dynamic camera zoom
    It feels like it often makes movement look blurry when zooming in or out. I could just be seeing things because I'm tired though. Let me know what you guys think.

--- Scripts/Gameplay/dynamic_camera.gd
+++ Scripts/Gameplay/dynamic_camera.gd
@@ -8,6 +8,14 @@ static var instance: DynamicCamera
 # The factor at which distance affects camera speed
 @export var smoothing: float = 1
 @export var pois: Array[Node2D]
+## Enable/Disable the dynamic zoom
+@export var dynamic_zoom: bool = true
+## The distance at which the camera should start zooming out
+@export var dist_thresh: float = 50
+@export var min_zoom: float = 1.3
+@export var max_zoom: float = 2
+## The amount that zoom changes with each unit of distance
+@export var zoom_slope: float = .0025
 # The distance at which the camera should stop before a border
 @export var limit_padding:float = 30
 @export var north_border: Node2D
@@ -26,12 +34,22 @@ func _ready() -> void:
 	limit_left = int(west_border.global_position.x + limit_padding)
 	limit_right = int(east_border.global_position.x - limit_padding)
 	limit_bottom = int(south_border.global_position.y - limit_padding)
+	zoom = Vector2(max_zoom, max_zoom)
 
 # Moves camera
 func _process(delta: float) -> void:
+	# POSITION
 	var goal: Vector2 = _avg_between_pois()
 	var dist: float = position.distance_to(goal)
 	position = position.move_toward(goal, dist * delta / smoothing)
+	# ZOOM
+	if(!dynamic_zoom): return
+	var stretch: float = position.distance_to(pois[0].position)
+	if(stretch > dist_thresh):
+		var newZoom : float = max_zoom - (stretch - dist_thresh) * zoom_slope
+		newZoom = clamp(newZoom, min_zoom, max_zoom)
+		zoom = Vector2(newZoom, newZoom)
+	
 
 func _avg_between_pois() -> Vector2:
 	var sum: Vector2 = Vector2(0, 0)
